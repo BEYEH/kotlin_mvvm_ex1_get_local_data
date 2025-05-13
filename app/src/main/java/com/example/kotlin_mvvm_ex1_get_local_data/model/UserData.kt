32cee3807238df13c1f462e5f5aaac0a64f16584
Kells Ping <kells.ping@quanta.corp-partner.google.com>
2025-05-13 19:30:07 +0800

    Create files for model.

---
+++ app/src/main/java/com/example/kotlin_mvvm_ex1_get_local_data/model/UserData.kt
@@ -0,0 +1,3 @@
+package com.example.kotlin_mvvm_ex1_get_local_data.model
+
+data class UserData()
---
+++ app/src/main/java/com/example/kotlin_mvvm_ex1_get_local_data/model/UserRepository.kt
@@ -0,0 +1,4 @@
+package com.example.kotlin_mvvm_ex1_get_local_data.model
+
+class UserRepository {
+}
